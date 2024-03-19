990b1d5769bdd76212b077f2f1b02df8a28dbf16
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2024-03-19 17:33:22 +0100

    asd

--- utils/unvalidated/src/lib.rs
+++ utils/unvalidated/src/lib.rs
@@ -3,6 +3,18 @@
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
 #![cfg_attr(not(test), no_std)]
+#![cfg_attr(
+    not(test),
+    deny(
+        clippy::indexing_slicing,
+        clippy::unwrap_used,
+        clippy::expect_used,
+        clippy::panic,
+        clippy::exhaustive_structs,
+        clippy::exhaustive_enums,
+        missing_debug_implementations,
+    )
+)]
 
 #[cfg(feature = "alloc")]
 extern crate alloc;
